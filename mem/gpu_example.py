06f2576e24c3c5a678bc2f573246077b579fab02
cydia2001 <shizukuprprpr@gmail.com>
2023-10-19 01:46:38 +0800

    new: add gpu example

---
+++ mem/gpu_example.py
@@ -0,0 +1,102 @@
+from cuda import cuda, nvrtc
+import numpy as np
+
+def ASSERT_DRV(err):
+    if isinstance(err, cuda.CUresult):
+        if err != cuda.CUresult.CUDA_SUCCESS:
+            raise RuntimeError("Cuda Error: {}".format(err))
+    elif isinstance(err, nvrtc.nvrtcResult):
+        if err != nvrtc.nvrtcResult.NVRTC_SUCCESS:
+            raise RuntimeError("Nvrtc Error: {}".format(err))
+    else:
+        raise RuntimeError("Unknown error type: {}".format(err))
+    
+saxpy = """\
+extern "C" __global__
+void saxpy(float a, float *x, float *y, float *out, size_t n) {
+    size_t tid = blockIdx.x * blockDim.x + threadIdx.x;
+    if (tid < n) {
+        out[tid] = a * x[tid] + y[tid];
+    }
+}
+"""
+
+err, prog = nvrtc.nvrtcCreateProgram(str.encode(saxpy), b"saxpy.cu", 0, [], [])
+
+opts = [b"--fmad=false", b"--gpu-architecture=compute_75"]
+err,  = nvrtc.nvrtcCompileProgram(prog, len(opts), opts)
+
+err, ptxSize = nvrtc.nvrtcGetPTXSize(prog)
+ptx = b" " * ptxSize
+err, = nvrtc.nvrtcGetPTX(prog, ptx)
+
+err, = cuda.cuInit(0)
+err, cuDevice = cuda.cuDeviceGet(0)
+err, context = cuda.cuCtxCreate(0, cuDevice)
+
+ptx = np.char.array(ptx)
+err, module = cuda.cuModuleLoadData(ptx.ctypes.data)
+ASSERT_DRV(err)
+err, kernel = cuda.cuModuleGetFunction(module, b"saxpy")
+ASSERT_DRV(err)
+
+NUM_THREADS = 512  # Threads per block
+NUM_BLOCKS = 32768  # Blocks per grid
+
+a = np.array([2.0], dtype=np.float32)
+n = np.array(NUM_THREADS * NUM_BLOCKS, dtype=np.uint32)
+bufferSize = n * a.itemsize
+
+hX = np.random.rand(n).astype(dtype=np.float32)
+hY = np.random.rand(n).astype(dtype=np.float32)
+hOut = np.zeros(n).astype(dtype=np.float32)
+
+err, dXclass = cuda.cuMemAlloc(bufferSize)
+err, dYclass = cuda.cuMemAlloc(bufferSize)
+err, dOutclass = cuda.cuMemAlloc(bufferSize)
+
+err, stream = cuda.cuStreamCreate(0)
+
+err, = cuda.cuMemcpyHtoDAsync(
+   dXclass, hX.ctypes.data, bufferSize, stream
+)
+err, = cuda.cuMemcpyHtoDAsync(
+   dYclass, hY.ctypes.data, bufferSize, stream
+)
+
+dX = np.array([int(dXclass)], dtype=np.uint64)
+dY = np.array([int(dYclass)], dtype=np.uint64)
+dOut = np.array([int(dOutclass)], dtype=np.uint64)
+
+args = [a, dX, dY, dOut, n]
+args = np.array([arg.ctypes.data for arg in args], dtype=np.uint64)
+
+err, = cuda.cuLaunchKernel(
+   kernel,
+   NUM_BLOCKS,  # grid x dim
+   1,  # grid y dim
+   1,  # grid z dim
+   NUM_THREADS,  # block x dim
+   1,  # block y dim
+   1,  # block z dim
+   0,  # dynamic shared memory
+   stream,  # stream
+   args.ctypes.data,  # kernel arguments
+   0,  # extra (ignore)
+)
+
+err, = cuda.cuMemcpyDtoHAsync(
+   hOut.ctypes.data, dOutclass, bufferSize, stream
+)
+err, = cuda.cuStreamSynchronize(stream)
+
+hZ = a * hX + hY
+if not np.allclose(hOut, hZ):
+   raise ValueError("Error outside tolerance for host-device vectors")
+
+err, = cuda.cuStreamDestroy(stream)
+err, = cuda.cuMemFree(dXclass)
+err, = cuda.cuMemFree(dYclass)
+err, = cuda.cuMemFree(dOutclass)
+err, = cuda.cuModuleUnload(module)
+err, = cuda.cuCtxDestroy(context)
